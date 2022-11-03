b2b0cd58e5ab5742dad0374b3aff052922e5f001
sherly600 <dj_niit@126.com>
2022-11-03 10:37:32 +0800

    nettytijiao

---
+++ src/main/java/com/perfect/netty/chat/server/handler/TerminalServerHandler.java
@@ -0,0 +1,31 @@
+package com.perfect.netty.chat.server.handler;
+
+import com.perfect.netty.chat.processor.MsgProcessor;
+import com.perfect.netty.chat.protocol.IMMessage;
+import lombok.extern.slf4j.Slf4j;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.json.simple.JSONObject;
+
+@Slf4j
+public class TerminalServerHandler extends SimpleChannelInboundHandler<IMMessage>{
+
+	private MsgProcessor processor = new MsgProcessor();
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, IMMessage msg) throws Exception {
+	    processor.sendMsg(ctx.channel(), msg);
+    }
+
+    /**
+     * 异常处理
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.info("Socket Client: 与客户端断开连接:" + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
